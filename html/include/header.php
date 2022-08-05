99d52388797b15c8d704d62e0dfa27a09c70372b
huzaifa iqbal <huzaifaiqbal89@gmail.com>
2022-08-06 00:29:44 +0500

    revision updated chat page created

--- html/include/header.php
+++ html/include/header.php
@@ -16,5 +16,6 @@
     <link rel="stylesheet" href="css/slick-theme.min.css" />
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&amp;display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/emoji.css">
     <link rel="stylesheet" href="css/custom.min.css" />
     <link rel="stylesheet" href="css/responsive.css" />
