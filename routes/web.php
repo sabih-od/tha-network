e66e22e4ce89f7f7d2983fcc98a5b067b0a2761b
xfshahrukh44 <ssidduit@gmail.com>
2022-10-04 17:36:34 +0500

    admin portal, set grades and goals

--- routes/web.php
+++ routes/web.php
@@ -40,8 +40,28 @@ Route::get('del/redis', function () {
     dd(\Illuminate\Support\Facades\Redis::del('test:key'));
 });
 
-require "auth.php";
+//ADMIN LOGIN
+Route::get('/admin/login', function () {
+    return view('admin.auth.login');
+})->middleware('guest')->name('admin.login');
 
+Route::namespace('App\Http\Controllers\Admin')->prefix('/admin')->middleware('admin')->group(function () {
+    //Dashboard
+    Route::get('dashboard', 'AdminController@dashboard')->name('dashboard');
+
+    //setting
+    Route::match(['get', 'post'], '/settings', 'SettingController@index')->name('settings');
+
+    //goal
+    Route::match(['get', 'post'], '/goals', 'GoalController@index')->name('admin.goals');
+
+    // Customer
+    Route::resource('customers', 'CustomersController');
+    Route::delete('/customers/destroy/{id}', 'CustomersController@destroy')->name('customers.destroy');
+});
+
+//Inertia routes
+require "auth.php";
 Route::group([
     'middleware' => ['auth', 'revalidate']
 ], function () {
