8cf9bd5b1718bf037934c817a58528fc3a1ff34f
samgarcia14433 <108543947+samgarcia14433@users.noreply.github.com>
2022-11-09 04:53:45 +0500

    revision updated

--- resources/js/components/HowItWorksMenu.vue
+++ resources/js/components/HowItWorksMenu.vue
@@ -18,30 +18,31 @@
                         <li class="ml-auto"><a href="https://www.facebook.com/Tha-Network-150057600527324/" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
                         <li><a href="https://twitter.com/ThaNetwork4" target="_blank"><i class="fab fa-twitter"></i></a></li>
                         <li><a href="https://www.instagram.com/_thanetwork_/" target="_blank"><i class="fab fa-instagram"></i></a></li>
-<!--                        <li><a href="#" target="_blank"><i class="fab fa-linkedin-in"></i></a></li>-->
+                        <li><a href="https://www.tiktok.com/@_thanetwork_?lang=en" target="_blank"><img style="filter: contrast(0)" :src="asset('images/simple-tiktok.png')" alt=""></a></li>
+                        <li><a href="https://www.youtube.com/channel/UCBf0MeQqY_T1Oqtw2qOK7Fg" target="_blank"><i class="fab fa-youtube"></i></a></li>
                     </ul>
                     <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active">
+                        <li :class="url === 'work'? 'nav-item active' : 'nav-item'">
                             <a class="nav-link" :href="this.$route('work')">How It Works</a>
                         </li>
-                        <li class="nav-item">
+                        <li :class="url === 'benefits'? 'nav-item active' : 'nav-item'">
                             <a class="nav-link" :href="this.$route('benefits')">Membership Benefits</a>
                         </li>
-                        <li class="nav-item">
+                        <li :class="url === 'about'? 'nav-item active' : 'nav-item'">
                             <a class="nav-link" :href="this.$route('about')">About Us</a>
                         </li>
-                        <li class="nav-item">
+                        <li :class="url === 'terms'? 'nav-item active' : 'nav-item'">
                             <a class="nav-link" :href="this.$route('terms')">Terms & Conditions</a>
                         </li>
-                        <li class="nav-item">
+                        <li :class="url === 'privacy'? 'nav-item active' : 'nav-item'">
                             <a class="nav-link" :href="this.$route('privacy')">Privacy Policy</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" :href="this.$route('about')">Contact Us</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" :href="this.$route('login')">Login</a>
-                        </li>
+<!--                        <li :class="$Link === 'about'? 'nav-item active' : 'nav-item'">-->
+<!--                            <a class="nav-link" :href="this.$route('about')">Contact Us</a>-->
+<!--                        </li>-->
+<!--                        <li :class="url === 'login'? 'nav-item active' : 'nav-item'">-->
+<!--                            <a class="nav-link" :href="this.$route('login')">Login</a>-->
+<!--                        </li>-->
                     </ul>
                 </div>
             </nav>
@@ -50,15 +51,22 @@
 </template>
 
 <script>
-import {Link} from "@inertiajs/inertia-vue3";
+import {Link, useForm, usePage} from "@inertiajs/inertia-vue3";
 import utils from "../mixins/utils";
+import {useToast} from "vue-toastification";
 
 export default {
     name: "HowItWorksMenu",
     mixins: [utils],
     components: {
         Link
-    }
+    },
+    data() {
+        return {
+            url : window.location.href.split('/')[-1],
+            page: usePage().component.value
+        }
+    },
 }
 </script>
 
