08d8b41f9558ba39b9522a5db1005860b5ecc82b
sabihOD <sheikh.muhammad@olivedigital.com>
2023-02-23 10:40:30 +0000

    Develop

--- resources/js/Pages/Contact.vue
+++ resources/js/Pages/Contact.vue
@@ -83,7 +83,7 @@
             <div class="container-md">
                 <div class="row">
                     <div class="col-lg-12 col-md-12 col-sm-12 wow fadeInLeft" data-wow-delay="1.2s">
-                        <a href="index.php" class="d-block text-center"><img :src="asset('images/logo.png')" alt="logo"></a>
+                        <a href="#" class="d-block text-center"><img :src="asset('images/logo.png')" alt="logo"></a>
                         <ul class="links">
                             <li><a href="#">How It Works</a></li>
                             <li><a href="#">Membership Benefits</a></li>
