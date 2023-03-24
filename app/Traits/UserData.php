db0eebf7e322c74b7fde4009324d801c0108be10
sabihOD <sheikh.muhammad@olivedigital.com>
2023-03-24 20:42:30 +0000

    Avatar: if the male avatar is the default settings allow a male avatar to be...

--- app/Traits/UserData.php
+++ app/Traits/UserData.php
@@ -237,10 +237,13 @@ trait UserData
     protected function getNetworkMemberssData(Request $request)
     {
         $user_id = $request->has('user_id') ? $request->get('user_id') : Auth::id();
-        $network = Network::where('user_id', $user_id)->whereHas('members', function($q) {
-            return $q->whereHas('user', function($q) {
-                return $q->whereNull('closed_on');
-            });
+//        $network = Network::where('user_id', $user_id)->whereHas('members', function($q) {
+//            return $q->whereHas('user', function($q) {
+//                return $q->whereNull('closed_on');
+//            });
+//        })->first();
+        $network = Network::where('user_id', $user_id)->whereHas('user', function($q) {
+            return $q->whereNull('closed_on');
         })->first();
         $network_members = $network ? $network->members()->get() : [];
         $network_member_ids = [];
