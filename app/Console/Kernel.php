bdedada18421a437d5162f99b817b697b9640fef
sabihOD <sheikh.muhammad@olivedigital.com>
2023-05-30 18:08:40 +0000

    Update file Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -21,9 +21,9 @@ class Kernel extends ConsoleKernel
 //            $schedule->call(function () {payment_not_made();})->monthlyOn($date, '00:00');
 //        }
         $schedule->call(function () {payment_not_made();})->daily();
-        $schedule->call(function () {commission_distribution();})->everyTenMinutes();
+        $schedule->call(function () {commission_distribution();})->everyThirtyMinutes();
 //        $schedule->call(function () {close_accounts();})->monthlyOn(16, '00:00');
-        $schedule->call(function () {close_accounts();})->everyTenMinutes();
+        $schedule->call(function () {close_accounts();})->everyThirtyMinutes();
         //testing | suspend and close accounts
 
         //monthly add goals
