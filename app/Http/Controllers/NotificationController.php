ca181e8be72c15e320efcbca3aa63d2caa06f757
sabihOD <sheikh.muhammad@olivedigital.com>
2023-06-13 02:50:59 +0500

    offset issue

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -30,6 +30,8 @@ class NotificationController extends Controller
         try {
             $notification = Notification::find($request->notification_id);
 
+            $notification->delete();
+
             return redirect(url()->previous(true))->with('success', "Notification deleted.");
         } catch (\Exception $e) {
             return redirect(url()->previous(true))->with('error', $e->getMessage());
