3c8635268724294cac7d86d2c09bcc196ed1f979
xfshahrukh44 <ssidduit@gmail.com>
2023-12-05 03:10:17 +0500

    api middleware

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -15,7 +15,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
+        $this->middleware('api2', ['except' => ['login']]);
     }
 
     public function login(Request $request)
