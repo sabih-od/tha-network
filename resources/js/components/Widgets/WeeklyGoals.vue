499065385e6e586fca6c798675f1692443e1b9a1
xfshahrukh44 <ssidduit@gmail.com>
2024-03-23 04:53:49 +0500

    Set week goals heading statically

--- resources/js/components/Widgets/WeeklyGoals.vue
+++ resources/js/components/Widgets/WeeklyGoals.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="cardWrap">
         <div class="weekly-goals">
-            <h2>Weekly Goals <span>{{ goals.weekly_goals }}</span></h2>
+<!--            <h2>Weekly Goals <span>{{ goals.weekly_goals }}</span></h2>-->
+            <h2>Weekly Goals <span> 25 </span></h2>
             <h3>Referrals Made <span class="text-success">{{ goals.referrals_made }}</span></h3>
             <h4>Weekly List</h4>
             <p v-for="item in goals.weekly_list">
