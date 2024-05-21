5b2d46791e2cc979301f6d24642c49438da734b8
hamzaakhtar901 <135383531+hamzaakhtar901@users.noreply.github.com>
2024-05-22 01:51:25 +0500

    confirm email field on registration (#107) (#108)
    Co-authored-by: Shayan <shayan.khan@olivedigital.com>

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -31,6 +31,7 @@ use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
 
 class RegisterController extends Controller
@@ -227,6 +228,9 @@ class RegisterController extends Controller
         $this->validator($request->all())->validate();
 
         $req = $request->all();
+        if ($req['email'] !== $req['confirm_email']) {
+           return redirect()->back()->with('error', 'confirm_email and email must be same');
+        }
 
             if (isset($req['stripe_subscription_id'])){
                 $checkPayment = Payment::where('stripe_checkout_session_id', $req['stripe_subscription_id'])->first();
